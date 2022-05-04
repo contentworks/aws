454376867af81d97555028d46562b633d658ff9f
cwauthor <82058689+cwauthor@users.noreply.github.com>
2022-05-04 15:22:23 +0530

    Create l04t01-tg01vm01-userdata.sh

---
+++ saa/labs/l04t01-tg01vm01-userdata.sh
@@ -0,0 +1,12 @@
+yum install httpd wget git
+systemctl restart httpd
+cat << 'EOF' >> /var/www/html/index.html
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+    <title>AWSome Website</title>
+</head>
+<body>
+  <h1>Hello! From Target Group 01 / VM 01</h1>
+</body>
+</html>
+EOF
