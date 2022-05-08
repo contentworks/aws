87060dacd80bf0ce5a936b466e4713277a4bb5c9
cwauthor <82058689+cwauthor@users.noreply.github.com>
2022-05-08 11:58:25 +0530

    Update l05t01-cafeapp-userdata.sh

--- saa/labs/l05t01-cafeapp-userdata.sh
+++ saa/labs/l05t01-cafeapp-userdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-yum install httpd wget git unzip -y
+amazon-linux-extras install epel -y
+yum install httpd wget git unzip stress -y
 systemctl restart httpd
 curl -O https://www.tooplate.com/zip-templates/2121_wave_cafe.zip
 unzip 2121_wave_cafe.zip
